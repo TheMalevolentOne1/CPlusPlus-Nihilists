622f273d32f4922b6fa795830b792d7208b3e27f
TheMalevolentOne1 <kylerobinson818@gmail.com>
2024-07-24 13:55:38 +0100

    ali files

---
+++ Aliyah/Programming-Related Work/Practice Challenges/C Plus Plus/Challenge 10.py
@@ -0,0 +1,15 @@
+'''
+Challenge 10
+
+Write a game that:
+• allows the user to play rock, paper,
+scissors against the computer
+• must display the computer’s choice
+and show the result of the game
+
+Tip: The computer’s answer must be random.
+
+Additional Resources for challenge:
+Entire Random Module Python (W3Schools): https://www.w3schools.com/python/module_random.asp
+Randint (Randon Module Function): https://www.w3schools.com/python/ref_random_randint.asp
+'''
---
+++ Aliyah/Programming-Related Work/Practice Challenges/C Plus Plus/Challenge 4.py
@@ -0,0 +1,9 @@
+'''
+Challenge 4
+
+Write a program that:
+• asks the user to input two numbers
+• divides the first number by the second
+number
+• outputs the result
+'''
